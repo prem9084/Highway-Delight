d0b011eba35b54ec92ed252d4625fbd868488956
prem9084 <146803260+prem9084@users.noreply.github.com>
2024-08-19 12:26:43 +0530

    second

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://otp-reset-auth.netlify.app", credentials: true })
+);
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
@@ -22,14 +24,14 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
+  connectDB();
+  console.log("Server is running on port: ", PORT);
 });
